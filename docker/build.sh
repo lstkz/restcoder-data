dcb1f26e92a9e3555370e62b0905c22dc37d20b1
sky <sky@Mac-Pro-sky.local>
2016-05-08 09:58:41 +0200

    use public docker registry

--- docker/build.sh
+++ docker/build.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 #BASE
-docker build -t docker.restcoder.com/base_trusty data/base/trusty
+docker build -t restcoder/base_trusty data/base/trusty
 
 #SERVICES
-docker build -t docker.restcoder.com/service_mongodb  data/service_mongodb/src
-docker build -t docker.restcoder.com/service_redis  data/service_redis/src
-docker build -t docker.restcoder.com/service_postgres  data/service_postgres/src
+docker build -t restcoder/service_mongodb:3.2.0  data/service_mongodb/3.2.0
+docker build -t restcoder/service_mysql:5.1.73  data/service_mysql/5.1.73
+docker build -t restcoder/service_postgres:8.4.20  data/service_postgres/8.4.20
+docker build -t restcoder/service_redis:3.0.6  data/service_redis/3.0.6
 
 
 #NODEJS
-docker build -t docker.restcoder.com/lang_nodejs:4.2.4  data/languages/nodejs/4.2.4
-docker build -t docker.restcoder.com/lang_nodejs:5.4.1  data/languages/nodejs/5.4.1
+docker build -t restcoder/lang_nodejs:4.2.4  data/languages/nodejs/4.2.4
+docker build -t restcoder/lang_nodejs:5.4.1  data/languages/nodejs/5.4.1
 
--- docker/push.sh
+++ docker/push.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 #BASE
-docker push docker.restcoder.com/base_trusty
+docker push restcoder/base_trusty
 
 #SERVICES
-docker push docker.restcoder.com/service_mongodb
-docker push docker.restcoder.com/service_redis
-docker push docker.restcoder.com/service_postgres
+docker push restcoder/service_mongodb:3.2.0
+docker push restcoder/service_mysql:5.1.73
+docker push restcoder/service_postgres:8.4.20
+docker push restcoder/service_redis:3.0.6
 
 #NODEJS
-docker push docker.restcoder.com/lang_nodejs:4.2.4
-docker push docker.restcoder.com/lang_nodejs:5.4.1
+docker push restcoder/lang_nodejs:4.2.4
+docker push restcoder/lang_nodejs:5.4.1
